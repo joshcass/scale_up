e1a6040198fd1bd2c34ab341db5d78ed8e553f21
Josh Cass <joshuacass@gmail.com>
2015-09-13 19:31:04 -0600

    Including L Reqquests with category exceepd mem limit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
   def show
-    @category = Category.includes(:loan_requests).find(params[:id])
+    @category = Category.find(params[:id])
     @categories = Category.all
     @loan_requests = @category.loan_requests.paginate(:page => params[:page], :per_page => 30)
   end
