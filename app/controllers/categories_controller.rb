88e20640070caf898ca2967ffc0a56fe287dddf7
Josh Cass <joshuacass@gmail.com>
2015-09-13 19:22:21 -0600

    Small query optimization

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
   def show
-    @category = Category.find(params[:id])
+    @category = Category.includes(:loan_requests).find(params[:id])
     @categories = Category.all
     @loan_requests = @category.loan_requests.paginate(:page => params[:page], :per_page => 30)
   end
