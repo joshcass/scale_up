d34c1a2ffd49d2836087ef580567abe8afb16f3d
Josh Cass <joshuacass@gmail.com>
2015-09-13 19:12:32 -0600

    Implemented categories with pagination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,8 @@
 class CategoriesController < ApplicationController
-  def index
-    @category = Category.all
-  end
 
   def show
     @category = Category.find(params[:id])
+    @categories = Category.all
+    @loan_requests = @category.loan_requests.paginate(:page => params[:page], :per_page => 30)
   end
 end
